407613ff0ee032e4f2c277a16e441eda1c077299
Maeva DTC <delruemaeva@orange.fr>
2024-08-20 17:29:23 +0200

    fixed header

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,25 @@ import data from "../data.json";
 import netflix from "../src/assets/netflix-logo.png";
 
 const App = () => {
-  <header>
-    <img src={netflix} />
-  </header>;
-  const dataArray = data.map((elem) => {
-    return (
-      <section>
-        <h2 key={elem.category}>{elem.category}</h2>
-        <article>
-          {elem.images.map((elem) => {
-            return <img src={elem} />;
-          })}
-        </article>
-      </section>
-    );
-  });
-
-  return dataArray;
+  return (
+    <>
+      <header>
+        <img src={netflix} />
+      </header>
+      {data.map((elem) => {
+        return (
+          <section>
+            <h2 key={elem.category}>{elem.category}</h2>
+            <article>
+              {elem.images.map((elem) => {
+                return <img src={elem} />;
+              })}
+            </article>
+          </section>
+        );
+      })}
+    </>
+  );
 };
 
 export default App;
